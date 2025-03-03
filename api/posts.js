4b87611309e544d5bb96c3978334563a23bac4b4
theEndless11 <aaa380144@gmail.com>
2025-03-03 17:38:33 +0530

    Update posts.js

--- api/posts.js
+++ api/posts.js
@@ -109,6 +109,3 @@ module.exports = async function handler(req, res) {
     return res.status(405).json({ message: 'Method not allowed' });
 };
 
-    return res.status(405).json({ message: 'Method not allowed' });
-};
-
