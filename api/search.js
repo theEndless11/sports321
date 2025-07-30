154c8a2b9f537b20b02ed5443fbea98f60710efa
theEndless11 <aaa380144@gmail.com>
2025-07-30 12:26:14 +0530

    Update search.js

--- api/search.js
+++ api/search.js
@@ -80,7 +80,7 @@ module.exports = async function handler(req, res) {
             // Fetch user details with updated counts
             const userQuery = `
                 SELECT 
-                    id, created_at, hobby, profile_picture, description,
+                    id, created_at, Music, profile_picture, description,
                     followers_count AS followersCount,
                     following_count AS followingCount,
                     friends_count AS friendsCount
